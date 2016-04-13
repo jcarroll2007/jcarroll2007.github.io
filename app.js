d381ee5bcd44075d89a22e94fae0a5bfd05c2967
Jordan Carroll <jcarroll2007@gmail.com>
2016-04-12 21:32:42 -0400

    updating.

--- app.js
+++ app.js
@@ -68,12 +68,19 @@
     }
     config.$inject = ["$stateProvider", "$urlRouterProvider"];
 
+    function init($rootScope) {
+        $rootScope.debug = true;
+    }
+    init.$inject = ["$rootScope"];
+
     angular.module('app', [
         'app.share',
+        'app.reasons',
         'ui.router',
         'ngAnimate'
     ])
-        .config(config);
+        .config(config)
+        .run(init);
 }());
 (function () {
     'use strict';
@@ -93,6 +100,30 @@
     angular.module('models', [
         'models.submission'
     ]);
+}());
+(function () {
+    'use strict';
+
+    function stateConfig($stateProvider) {
+        $stateProvider
+            .state('app.reasons', {
+                controller: ["submissions", function (submissions) {
+                    this.submissions = submissions;
+                }],
+                controllerAs: 'reasonsState',
+                resolve: {
+                    submissions: ["Submission", function (Submission) {
+                        return Submission.getList();
+                    }]
+                },
+                template: '<reasons-list ng-model="reasonsState.submissions"></reasons-list>',
+                url: 'reasons'
+            });
+    }
+    stateConfig.$inject = ["$stateProvider"];
+
+    angular.module('app.reasons', ['app.reasons.list', 'app.reasons.reason'])
+        .config(stateConfig);
 }());
 (function () {
     'use strict';
@@ -173,6 +204,10 @@
             list: []
         };
 
+        self.getList = function () {
+            return $q.resolve(self._cache.list);
+        };
+
         self.save = function (model) {
             /**
              * This function currently just creates a fake submission
@@ -189,8 +224,121 @@
     }
     Submission.$inject = ["$q"];
 
+    function loadFixtures($rootScope, Submission) {
+        if ($rootScope.debug) {
+            Submission.save({
+                id: 1,
+                name: 'Jordan Carroll',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven',
+                image: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/12342504_10208675592032047_2237316667569290936_n.jpg?oh=66cfdbe0967533875b661dbf8281e94b&oe=574BE051'
+            });
+            Submission.save({
+                id: 2,
+                name: 'Zack Browne',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD because He caerve it. He made a way from Heaven, not to Heaven I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to HeavenI love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven',
+                image: 'http://media.idownloadblog.com/wp-content/uploads/2012/04/Phil-Schiller-headshot-e1362692403868.jpg'
+            });
+            Submission.save({
+                id: 50,
+                name: 'John Doe',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven',
+                image: 'http://pdnpulse.pdnonline.com/wp-content/uploads/2012/07/Andrew-Fingerman-Headshot.jpg'
+            });
+            Submission.save({
+                id: 25,
+                name: 'Alan Thicke',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD bm Heaven, not to Heaven',
+                image: 'https://avatars1.githubusercontent.com/u/8485105?v=3&s=460'
+            });
+            Submission.save({
+                id: 5,
+                name: 'Jordan Carroll',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven',
+                image: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/12342504_10208675592032047_2237316667569290936_n.jpg?oh=66cfdbe0967533875b661dbf8281e94b&oe=574BE051'
+            });
+            Submission.save({
+                id: 6,
+                name: 'Jordan Carroll',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven',
+                image: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/12342504_10208675592032047_2237316667569290936_n.jpg?oh=66cfdbe0967533875b661dbf8281e94b&oe=574BE051'
+            });
+        }
+    }
+    loadFixtures.$inject = ["$rootScope", "Submission"];
+
     angular.module('models.submission', [])
-        .factory('Submission', Submission);
+        .factory('Submission', Submission)
+        .run(loadFixtures);
+}());
+(function () {
+    'use strict';
+
+    function ReasonsController() {
+        var ctrl = this;
+        ctrl.reasons = [];
+
+        ctrl.add = function () {
+            ctrl.ngModel.unshift({
+                name: 'Jordan Carroll',
+                email: 'jcarroll2007@gmail.com',
+                age: '23',
+                reason: 'I love our LORD because He came to us to save us when we didn\'t deserve it. He made a way from Heaven, not to Heaven',
+                image: 'https://scontent-atl3-1.xx.fbcdn.net/hphotos-xta1/v/t1.0-9/12342504_10208675592032047_2237316667569290936_n.jpg?oh=66cfdbe0967533875b661dbf8281e94b&oe=574BE051'
+            });
+        };
+    }
+
+    function reasonsList() {
+        return {
+            controller: ReasonsController,
+            controllerAs: 'ctrl',
+            bindToController: true,
+            restrict: 'E',
+            scope: {
+                ngModel: '='
+            },
+            templateUrl: 'app/reasons/list/list.html'
+        };
+    }
+
+    angular.module('app.reasons.list', ['akoenig.deckgrid'])
+        .directive('reasonsList', reasonsList);
+
+}());
+(function () {
+    'use strict';
+
+    function ReasonController() {
+        this.test = 'test';
+    }
+
+    function reason() {
+        return {
+            controller: ReasonController,
+            controllerAs: 'ctrl',
+            bindToController: {
+                ngModel: '=?'
+            },
+            restrict: 'E',
+            scope: {},
+            templateUrl: 'app/reasons/reason/reason.html'
+        };
+    }
+
+    angular.module('app.reasons.reason', [])
+        .directive('reason', reason);
 }());
 (function () {
     'use strict';
